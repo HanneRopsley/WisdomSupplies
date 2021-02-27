1ee7570ac27643a934c75a223d0eade58390315b
Hanne Ropsley <hanne_vincent@hotmail.com>
2021-02-27 17:04:08 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wisdom Supplies
+### This application supplies messages full of wisdom to the end user depending on how they’re feeling and what the context is
+**Coding languages**: html / css / javascript
+**Version control and sharing**: git / github
---
+++ script.js
@@ -0,0 +1,9 @@
+const showWisdom = () => {
+    document.getElementById("page1").style.display = "none";
+    document.getElementById("page2").style.display = "block";
+}
+
+const backHome = () => {
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("page1").style.display = "block";
+}
